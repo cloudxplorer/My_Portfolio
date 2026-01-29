da88690b78fa8fcfb94abfbd5f76e20205065e08
CloudXplorer <jk0347859@gmail.com>
2026-01-29 22:34:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let num1  = 5;
+let num2 = 10;
+
+if(num1< num2){
+    console.log(`${num1}is smaller then ${num2}`);
+
+}
+else{
+    console.log (`{num2}is larger then ${num1}`);
+}
+
+let arr1=[2,,4,6,8,10]
+console.log(arr1);
+arr1.push[12];
+console.log(arr1);
+
+
+arr1.pop();
+console.log(arr1);
+
+arr1.shift();
+console.log(arr1);
+
+arr1.unshift();
+console.log(arr1);
+
+
+for (let step = 0; step < 5 ; step++)
+     {
+         console.log("Walking east one step");
+     }
+
+     let fruits = ["Apple" ,"Mango","Orange"];
+for (let i = 0; i < fruits.length; i++)
+    {console.log("Fruit at index" + i+ ":" + fruits[i]);} 
+
+
+const name = "Pallavi" ;
+let age = 22 ;
+let isstudent = true;
+let hobbies = ["dancing ","coding" , "reading"];
+console.log(name);
+console.log(age);
+console.log(isstudent)
+console.log(hobbies[0])
